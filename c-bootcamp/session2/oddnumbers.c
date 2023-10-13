b848f650350e1e37ae77d2f074b0d04c43072132
Matthew Alwan <74419801+quaoz@users.noreply.github.com>
2023-10-13 15:50:30 +0000

    Session 2

---
+++ c-bootcamp/session2/oddnumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    
+    for (a = 1; a <= 19; a += 2) { 
+        printf("%d\n", a);
+    }
+    
+    return 0;    
+}
---
+++ c-bootcamp/session2/squares.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    
+    for (a = 1; a <= 10; a++) { 
+        printf("%d squared is %d\n", a, a * a);
+    }
+    
+    return 0;    
+}
