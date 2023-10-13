3c28873c8444d7bdb495fedafe5a870f9e3969e0
Matthew Alwan <74419801+quaoz@users.noreply.github.com>
2023-10-13 15:20:26 +0000

    Session 2

--- c-bootcamp/worksheet1/swapping.c
+++ c-bootcamp/worksheet1/swapping.c
@@ -4,9 +4,13 @@ int main() {
     int a = 1;
     int b = 2;
 
+    printf("a = %d, b = %d\n", a, b);
+
     int temp = a;
     a = b;
     b = temp;
+
+    printf("a = %d, b = %d\n", a, b);
 
     return 0;
 }
